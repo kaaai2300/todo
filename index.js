620c2a1f16e8f4c2a99e6adcae0c116e6c0c8597
佐藤魁 <satokai@satoukainoMBP.AirPort>
2021-08-31 23:25:46 +0900

    削除処理の追加

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
-const $button = document.getElementsByTagName('button')[0]
+const $addPlan = document.getElementById('add-plan')
+const $clearButton = document.getElementById('clear-plan')
 
-$button.addEventListener('click', (e) => {
+/**
+ * 予定の追加処理
+ */
+$addPlan.addEventListener('click', (e) => {
   // 入力値を取得
   const $plan = document.getElementById('js-plan')
   console.log($plan.value);
@@ -12,12 +16,27 @@ $button.addEventListener('click', (e) => {
   }
 })
 
+/**
+ * 削除処理
+ */
+$clearButton.addEventListener('click', (e) => {
+  const $allPlans = document.getElementsByClassName('plan')
+
+  if ($allPlans.length !== 0) {
+    for(let i = $allPlans.length - 1; i >= 0; i--) {
+      $allPlans[i].remove();
+    }
+  } else {
+    window.alert('予定を入力してください。')
+  }
+})
+
 const setPlanList = ($plan) => {
   // リストに追加
   let $planList = document.getElementsByTagName('ul')[0];
 	let $planItem = document.createElement("li");
 	$planItem.textContent = $plan.value;
-  $planItem.classList = "list-group-item"
+  $planItem.classList = "list-group-item plan"
 	$planList.appendChild($planItem);
   
   // 入力値の初期化
